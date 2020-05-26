d9bd7e4360c7824f9e3798f053c323fabbffae5a
yassinetimdouine <61888686+yassinetimdouine@users.noreply.github.com>
2020-05-26 23:45:04 +0200

    add port 80

--- hello-world/hello-world-python/launch.py
+++ hello-world/hello-world-python/launch.py
@@ -4,4 +4,5 @@ helloworld = Flask(__name__)
 def run():
     return "{\"message\":\"Hello World Python v1\"}"
 if __name__ == "__main__":
-    helloworld.run(host="0.0.0.0", debug=True)
+    helloworld.run(host="0.0.0.0", port=80, debug=True)
+    
